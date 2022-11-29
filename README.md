c9157b4557b794d8652c3250d26cb185da2952f7
qualk <kasunvup@gmail.com>
2022-11-30 06:50:26 +1100

    add lester and 2 copypastas

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # le bot
 
-A goofy ahh bot that sends dumb, overused memes on the slightest provocation. This included wenomechainsama and beast boy 4 meme. MOre will be included when I feel like it (I do not know how to code Python). It is coded in python using `nextcord`. 
+A goofy ahh bot that sends dumb, overused memes on the slightest provocation. This included wenomechainsama and beast boy 4 meme.
+It is really fkn simple just watch a yt video on discord.py and you can figure out the rest.
 
 # How to use
 
 To set up the bot go into a venv if u wish to keep it seperate from the rest of your python install and run
 On Linux/Mac:
 ```bash
-$ python3 -m pip install -U nextcord
+python3 -m pip install -U nextcord
 ```
 On Windows:
 ```console
-> pip install nextcord
+pip install nextcord
 ```
 
 Now go into the discord developer portal where you have hopefully set up a bot if you followed the instructions to watch a YouTube video about discord.py and get the TOKEN from there.
-Make a file named .env and type: `TOKEN=put token here, do not put apostrophe or brackets`
+Paste the TOKEN into the specified field in the .env file. If there is no .env file, make a file named `.env` and type: `TOKEN=`, then put the token right next to the equals (=) sign, no brackets, apostrophes or spaces.
 
 Now run main.py
 
-Happy goofing around!
+# Test this current version of the bot
+
+Test here: https://discord.gg/JdBeA7mW
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nextcord
+#from nextcord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -14,6 +15,7 @@ TOKEN = os.environ.get('TOKEN')
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await client.change_presence(status=nextcord.Status.idle, activity=nextcord.Game("with qualk's balls"))
 
 @client.event
 async def on_message(message):
@@ -31,9 +33,16 @@ async def on_message(message):
         
     elif message.content.startswith('$skill'):
         await message.channel.send('issue', reference=message)
+        
+    elif message.content.startswith('$lester'):
+        await message.channel.send(file=nextcord.File('assets\\images\\lester.jpg'), reference=message)
     
     elif 'fogor' in message.content.lower():
       await message.add_reaction('\N{skull}')
+      return
+    
+    elif 'yum' in message.content.lower():
+      await message.add_reaction('😋')
       return
   
     elif 'did i ask' in message.content.lower():
@@ -50,9 +59,15 @@ async def on_message(message):
         'Teacher I suspect the answer to your question is four teacher', reference=message)
       await message.channel.send(file=nextcord.File('assets\\images\\four.jpg'))
       return
+    
+    elif 'cena' in message.content.lower():
+      await message.channel.send('Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING 🥶🍦 wǒ hěn xǐhuān BING CHILLING 🥶🍦 dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING 🥶🍦 sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 bùyào wàngjì bùyào cu òguò jìdé qù diànyǐngyuàn kàn sùdù yǔ jīqíng 9 yīn wéi fēicháng hǎo diànyǐng dòngzuò fēicháng hǎo chàbùduō yīyàng BING CHILLING 🥶🍦zàijiàn 🥶🍦', reference=message)
+      return
+    
+    elif 'hot' in message.content.lower():
+      await message.channel.send('Fucking plastic chigger balls. Mid. Instead lf trying to be cool 🤐🤐😂 how about you invest in NFTs 😃💯 💵  earn some bands 🥶🥶😱😈😈 💵 and become an entrepreneur. 😴😎😎🥶🥶😴😴😪😪🥵🥵🥵🥶😎😎😎😎🥱🥱. Live the top G lifestyle mate. 😇😇😉😊🤩🤩😍😍🥵🥵🥵🥳🥳🥶🥶🥶🥶', reference=message)
   
     else:
       return
-
 
 client.run(os.getenv("TOKEN"))
